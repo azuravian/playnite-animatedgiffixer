e790bf39549ceb36bae4bb64ce79d4e772c7c735
azuravian <azuravian@gmail.com>
2024-07-28 23:41:44 -0400

    Added functions for tagging and previewing

--- AnimatedGifFixer.psm1
+++ AnimatedGifFixer.psm1
@@ -1,15 +1,18 @@
-function GetMainMenuItems
+function GetGameMenuItems
 {
     param(
-        $getMainMenuItemsArgs
+        $MenuArgs
     )
 
-    $menuItem1 = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
-    $menuItem1.Description = "Fix Animated Gifs"
-	$menuItem1.FunctionName = "FixAnimatedGifs"
-    $menuItem1.MenuSection = "@Game Media Tools"
-
-    return $menuItem1
+    $menuItem = New-Object Playnite.SDK.Plugins.ScriptGameMenuItem
+    $menuItem.Description = "Fix Animated GIFs"
+	$menuItem.FunctionName = "FixAnimatedGifs"
+	$menuItem.MenuSection = "Animated GIF Fixer"
+	$menuItem1 = New-Object Playnite.SDK.Plugins.ScriptGameMenuItem
+    $menuItem1.Description = "Tag Animated GIFs"
+	$menuItem1.FunctionName = "TagAnimatedGifs"
+	$menuItem1.MenuSection = "Animated GIF Fixer"
+    return $menuItem, $menuItem1
 }
 
 function FixAnimatedGifs
@@ -78,58 +81,245 @@ function FixAnimatedGifs
 	foreach ($game in $GameDatabase){
 		$global:description = $game.Description
 		$global:gamebool = $false
-		$regex = '(https?:\/\/)(.[^"]*?)(gif|png)(.*?)(?=")'
+		$regex = '(https?:\/\/)(.[^"]*?)(gif|png|jpg)(.*?)(?=")'
 		$RegexMatches = ([regex]$regex).Matches($global:description)
 		$imgnum = 0
-		
+		$tagMatch = $PlayniteApi.Database.tags.Add('[AGF]')
+		$tagId = $tagMatch.Id
 		foreach ($match in $RegexMatches){
 			$gifpath = New-TemporaryDirectory
 			if (!(Test-Path -Path $gifpath))
 			{
-				md -Path $gifpath
+				mkdir -Path $gifpath
 			}
 			
-			if ($match -like "*gif*"){
+			if ($match -like "*gif*" -Or $match -like "*jpg*"){
 				$tempgif = Join-Path $gifpath -ChildPath "temp.gif"
 			}
 			elseif ($match -like "*png*"){
 				$tempgif = Join-Path $gifpath -ChildPath "temp.png"
 			}
-			$targetpng = Join-Path $gifpath -ChildPath "target.png"
+			$guid = New-Guid
+			$guid = "$guid.png"
+			$targetpng = Join-Path $gifpath -ChildPath $guid
 			$images = @()
 			wget -O $tempgif $match.ToString()
-			if ($tempgif -like "*gif*"){
-				& "$MagickExecutablePath" convert -coalesce "$tempgif" "$targetpng"
-				$frames = & "$MagickExecutablePath" convert $tempgif"[-1]" -format %[scene] info:
+			if ($tempgif -like "*gif*")
+			{
+				$anigifs = & "$MagickExecutablePath" identify $tempgif
 			}
-			elseif ($tempgif -like "*png*"){
-				& "$MagickExecutablePath" convert -coalesce apng:"$tempgif" "$targetpng"
-				$frames = (Get-ChildItem $gifpath | Measure-Object).Count
+			elseif ($tempgif -like "*png*")
+			{
+				$anigifs = & "$MagickExecutablePath" identify apng:$tempgif
 			}
-			if ([int]$frames -lt 12){
+			$frames = [int]$anigifs.Count
+			if ($frames -lt 12)
+			{
 				continue
 			}
-			$div = [int][Math]::Floor([int]$frames / 12)
+			
+			$answer = Show-Gif
+			
+			if ($answer -eq 7)
+			{
+				continue
+			}
+			$div = [int][Math]::Floor($frames / 12)
 			$constdiv = $div
 			while ($div -lt $frames){
 				$img = "target-" + $div + ".png"
 				$img = Join-Path $gifpath $img
+				$delnum = $div - 1
+				& "$MagickExecutablePath" convert "$tempgif[0-$div]" -coalesce -delete "0-$delnum" $img
 				$images += $img
 				$div += $constdiv
 			}
 			OpenWindow $match $imgnum
 			$imgnum += 1
-			
 		}
 		$game.Description = $global:description
 		if ($global:gamebool -eq $true){
 			$gamecount += 1
+			$game.tagIds.Remove($tagId)
 		}
 		$total += 1
 		$PlayniteApi.Database.Games.Update($game)
 	}
 	Clear-Folder
 	$PlayniteApi.Dialogs.ShowMessage("$total selected games have been inspected. `n$gamecount games were updated. `nModified a total of $imgcount images.", "Fix Animated Gifs")
+}
+
+function TagAnimatedGifs
+{
+	param(
+        $scriptMainMenuItemActionArgs
+    )
+	# Load assemblies
+    Add-Type -AssemblyName PresentationCore
+    Add-Type -AssemblyName PresentationFramework
+	
+	$GameDatabase = $PlayniteApi.MainView.SelectedGames
+	$timer = $div = [int]($GameDatabase.Count / 60)
+	$gamecount = 0
+	$total = 0
+	$tagMatch = $PlayniteApi.Database.tags.Add('[AGF]')
+    $tagId = $tagMatch.Id
+	$PlayniteApi.Dialogs.ShowMessage("Based on the selected games, this process will take approximately $timer minutes.", "Fix Animated Gifs")
+	foreach ($game in $GameDatabase)
+	{
+		try {
+			$regex = '(https?:\/\/)(.[^"]*?)(gif|png|jpg)(.*?)(?=")'
+			$RegexMatches = ([regex]$regex).Matches($game.Description)
+			foreach ($match in $RegexMatches)
+			{
+				$gifpath = New-TemporaryDirectory
+				if (!(Test-Path -Path $gifpath))
+				{
+					mkdir -Path $gifpath
+				}
+				
+				if ($match -like "*gif*" -Or $match -like "*jpg*")
+				{
+					$tempgif = Join-Path $gifpath -ChildPath "temp.gif"
+				}
+				elseif ($match -like "*png*")
+				{
+					$tempgif = Join-Path $gifpath -ChildPath "temp.png"
+				}
+				
+				wget -O $tempgif $match.ToString()
+				
+				if ($tempgif -like "*gif*")
+				{
+					$anigifs = magick identify $tempgif
+				}
+				elseif ($tempgif -like "*png*")
+				{
+					$anigifs = magick identify apng:$tempgif
+				}
+				if ($anigifs.Count -gt 1)
+				{
+					if ($game.tagIds -notcontains $tagId)
+					{
+						$gamecount += 1
+						# Add tag Id to game
+						if ($game.tagIds)
+						{
+							$game.tagIds += $tagId
+						}
+						else
+						{
+							# Fix in case game has null tagIds
+							$game.tagIds = $tagId
+						}
+					}
+					$PlayniteApi.Database.Games.Update($game)
+					break
+				}
+			}
+			$total += 1
+		} catch {
+			continue
+		}
+		Clear-Folder
+	}
+	$PlayniteApi.Dialogs.ShowMessage("$total selected games have been inspected. `n$gamecount games appear to have animated gifs.  They have had the tag [AGF] added to them.", "Fix Animated Gifs")
+}
+
+function Show-Gif {	
+	$imgheight = & "$MagickExecutablePath" convert "$tempgif[0]" -format '%h' info:
+	$imgwidth = & "$MagickExecutablePath" convert "$tempgif[0]" -format '%w' info:
+	$windowheight = [int]$imgheight + 120
+	$windowwidth = [int]$imgwidth + 20
+	#Create a form
+	
+	$borderheight = [int]$imgheight + 4
+	$borderwidth = [int]$imgwidth + 4
+	$gifnum = $imgcount + 1
+
+	Add-Type -AssemblyName System.Windows.Forms
+	$Form = New-Object System.Windows.Forms.Form
+	$Form.Size = New-Object System.Drawing.Size($windowwidth, $windowheight)
+	$Form.StartPosition = "CenterScreen"
+	$Form.BackColor = "#FF262f39"
+	$Form.FormBorderStyle = "None"
+
+	#Adding some text
+	$Form.Text = "GIF Player"
+	
+	$answer = 7
+
+	$Panel = New-Object System.Windows.Forms.FlowLayoutPanel
+	$Panel.AutoScroll = $true
+	$Panel.AutoSize = $true
+	$Panel.FlowDirection = "TopDown"
+	$Panel.WrapContents = $false
+	$Panel.Margin = "2,2,2,2"
+	
+	$Border = New-Object System.Windows.Forms.FlowLayoutPanel
+	$Border.BackColor = "white"
+	$Border.Margin="8,8,8,8"
+	$Border.AutoSize = $true
+	
+	$ButtonPanel = New-Object System.Windows.Forms.FlowLayoutPanel
+	$ButtonPanel.Margin = "10,10,10,10"
+	
+	$Title = New-Object System.Windows.Forms.Label
+	$Title.Location = New-Object System.Drawing.Size(10,10)
+	$Title.AutoSize = $true
+	$Title.ForeColor = "white"
+	$Title.Font = New-Object System.Drawing.Font ("Courier",14, [System.Drawing.Fontstyle]::Bold)
+	$Title.Text = "Gif #$gifnum for $game"
+	$Title.Margin = "10,0,0,0"
+	$Panel.Controls.Add($Title)
+	
+	#Get the local saved GIF
+	$gifBox = New-Object Windows.Forms.picturebox
+	$gifLink= (Get-Item -Path $tempgif)
+	$img = [System.Drawing.Image]::fromfile($gifLink)
+	$gifBox.AutoSize = $true
+	$gifBox.Image = $img
+	$gifBox.Margin = "4,4,4,4"
+	$Border.Controls.Add($gifbox)
+	$Panel.Controls.Add($Border)
+	
+	$Label = New-Object System.Windows.Forms.Label
+	$Label.AutoSize = $true
+	$Label.ForeColor = "white"
+	$Label.Font = New-Object System.Drawing.Font ("Courier",10, [System.Drawing.Fontstyle]::Bold)
+	$Label.Text = "Do you want to process this gif?"
+	$Label.Margin = "10,0,0,0"
+	$Panel.Controls.Add($Label)
+	
+	$Button1 = New-Object System.Windows.Forms.Button
+	#$Button1.Location = New-Object System.Drawing.Point(10,400)
+	$Button1.Size = New-Object System.Drawing.Size(80,20)
+	$Button1.Text = "Yes"
+	$Button1.ForeColor = "white"
+	$Button1.BackColor = "#3287e3"
+	$ButtonPanel.Controls.Add($Button1)
+	
+	$Button = New-Object System.Windows.Forms.Button
+	#$Button.Location = New-Object System.Drawing.Point(100,400)
+	$Button.Size = New-Object System.Drawing.Size(80,20)
+	$Button.Text = "No"
+	$Button.ForeColor = "white"
+	$Button.BackColor = "#3287e3"
+	$ButtonPanel.Controls.Add($Button)
+	
+	$Panel.Controls.Add($ButtonPanel)
+	$Form.Controls.Add($Panel)
+	
+	$Button.Add_Click({
+		$Form.DialogResult = 7
+	})
+	
+	$Button1.Add_Click({
+		$Form.DialogResult = 6
+	})
+
+	#Execute the form
+	$Form.ShowDialog()
 }
 
 function New-TemporaryDirectory {
@@ -146,7 +336,7 @@ function Clear-Folder {
 	{
 		try
 		{
-			Remove-Item -ErrorAction Ignore -Recurse $FixAniGifFolder
+			Remove-Item -ErrorAction Ignore -Recurse -Force $FixAniGifFolder
 		}
 		catch
 		{
@@ -280,13 +470,12 @@ function OpenWindow($match, $imgnum)
 		$sel = $ListBoxImages.SelectedIndex.ToString()
 		$selpath = $images[$sel]
 		$newpath = Join-Path $CurrentExtensionDataPath -ChildPath $($game.Id)
-		$ni = $imgnum.ToString() + ".png"
-		$newfile = Join-Path $newpath -ChildPath $($ni)
-		
-		
+		$nip = "$($([System.IO.Path]::GetRandomFileName()).Split('.')[0]).png"
+		$newfile = Join-Path $newpath -ChildPath $($nip)
+				
 		if (!(Test-Path -Path $newpath))
 		{
-			md -Path $newpath
+			mkdir -Path $newpath
 		}
 		
 		
@@ -298,7 +487,7 @@ function OpenWindow($match, $imgnum)
 		$global:description = $global:description.replace($match, $newfile)
 		$global:imgcount += 1
 		$global:gamebool = $true
-		$Window.DialogResult = $true		
+		$Window.DialogResult = $true
 	})
 	
     
@@ -307,4 +496,3 @@ function OpenWindow($match, $imgnum)
     $Window.ShowDialog()
     $__logger.Info("Fix Animated Gifs - Window closed.")
 }
-
